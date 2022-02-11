b385904c65176b1e309e1dafcf254870bec99764
AdamPtk <adam.pietkiewicz12@gmail.com>
2022-02-11 09:27:01 +0100

    Cryptodetails layout created

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -28,10 +28,10 @@ const CryptoDetails = () => {
   const volume = () => {
     for (const [key, value] of Object.entries(cryptoDetails)) {
       if (key === '24hVolume') {
-        console.log(value)
         return value
       }
     }
+    return 0
   }
 
   const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
@@ -86,13 +86,55 @@ const CryptoDetails = () => {
             </p>
           </Col>
           {stats.map(({ icon, title, value }) => (
-            <Col className="coin-stats">
+            <Col className="coin-stats" key={title}>
               <Col className="coin-stats-name">
                 <Text>{icon}</Text>
                 <Text>{title}</Text>
               </Col>
               <Text className="stats">{value}</Text>
             </Col>
+          ))}
+        </Col>
+        <Col className="other-stats-info">
+          <Col className="coin-value-statistics-heading">
+            <Title level={3} className="coin-detailes-heading">
+              Other Statistics
+            </Title>
+            <p>
+              An overview showing the stats of all cryptocurrencies
+            </p>
+          </Col>
+          {genericStats.map(({ icon, title, value }) => (
+            <Col className="coin-stats" key={title}>
+              <Col className="coin-stats-name">
+                <Text>{icon}</Text>
+                <Text>{title}</Text>
+              </Col>
+              <Text className="stats">{value}</Text>
+            </Col>
+          ))}
+        </Col>
+      </Col>
+      <Col className="coin-desc-link">
+        <Row className="coin-desc">
+            <Title level={3} className="coin-details-heading">
+              What is {cryptoDetails?.name}
+              <div dangerouslySetInnerHTML={{__html: cryptoDetails?.description}}></div>
+            </Title>
+        </Row>
+        <Col className="coin-links">
+          <Title level={3} className="coin-details-heading">
+            {cryptoDetails?.name} Links
+          </Title>
+          {cryptoDetails?.links.map((link) => (
+            <Row className="coin-link" key={link.name}>
+              <Title level={5} className="link-name">
+                {link.type}
+              </Title>
+              <a href={link.url} target="_blank" rel="noreferrer">
+                {link.name}
+              </a>
+            </Row>
           ))}
         </Col>
       </Col>
