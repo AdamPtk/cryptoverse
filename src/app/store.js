d6738ce0b3c62dfb51119ed32ea91aa3aa6f0226
AdamPtk <adam.pietkiewicz12@gmail.com>
2024-05-25 14:11:21 +0200

    Middleware added

--- src/app/store.js
+++ src/app/store.js
@@ -6,6 +6,10 @@ import { cryptoNewsApi } from '../services/cryptoNewsApi';
 export default configureStore({
   reducer: {
     [cryptoApi.reducerPath]: cryptoApi.reducer,
-    [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer
-  }
-})
+    [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware()
+      .concat(cryptoApi.middleware)
+      .concat(cryptoNewsApi.middleware),
+});
